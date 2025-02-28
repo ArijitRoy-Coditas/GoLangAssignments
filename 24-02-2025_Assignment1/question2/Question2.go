3c13e23a9b536bf7318517b3598e32d8a406db3e
ArijitRoy-Coditas <arijit.roy@coditas.com>
2025-02-28 16:47:42 +0530

    Used HashMap instead slice of employee array for faster and efficient approach

--- 24-02-2025_Assignment1/question2/Question2.go
+++ 24-02-2025_Assignment1/question2/Question2.go
@@ -7,21 +7,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
+
 	"github.com/olekukonko/tablewriter"
 )
 
 type employee struct {
-	name string
-	age uint
+	name   string
+	age    uint
 	salary float64
 }
 
 type department struct {
-	name string
-	employees []employee
+	name      string
+	employees map[int]employee
 }
 
 func GetUserInput(message string) string {
@@ -42,7 +44,7 @@ func GetUserID(d *department, message string) uint64 {
 			continue
 		}
 
-		if strings.HasPrefix(input, "-"){
+		if strings.HasPrefix(input, "-") {
 			log.Println("Employee ID cannot be negative")
 			continue
 		}
@@ -66,66 +68,66 @@ func GetUserID(d *department, message string) uint64 {
 
 func NewEmployeeDetails() []string {
 	var name string
-    for {
-        name = GetUserInput("Enter the name of the new employee:")
+	for {
+		name = GetUserInput("Enter the name of the new employee:")
 		if name == "" {
 			log.Println("Name cannot be empty")
 			continue
 		}
-        if _, err := strconv.Atoi(name); err == nil {
-            log.Println("Employee name cannot be a number.")
+		if _, err := strconv.Atoi(name); err == nil {
+			log.Println("Employee name cannot be a number.")
 			continue
-        } else {
-            break
-        }
-    }
+		} else {
+			break
+		}
+	}
 
-    // Get and validate the employee's age
-    var age uint64
-    for {
-        input := GetUserInput("Enter the age of the new employee:")
-        if strings.HasPrefix(input, "-") {
-            log.Println("Age cannot be negative.")
-            continue
-        }
+	// Get and validate the employee's age
+	var age uint64
+	for {
+		input := GetUserInput("Enter the age of the new employee:")
+		if strings.HasPrefix(input, "-") {
+			log.Println("Age cannot be negative.")
+			continue
+		}
 
 		if input == "0" {
 			log.Println("Age cannot be zero")
 			continue
 		}
 
-        var err error
-        age, err = strconv.ParseUint(input, 10, 32)
-        if err != nil {
-            log.Println("Invalid age:", err)
-            continue
-        }
-        break
-    }
+		var err error
+		age, err = strconv.ParseUint(input, 10, 32)
+		if err != nil {
+			log.Println("Invalid age:", err)
+			continue
+		}
+		break
+	}
 
-    // Get and validate the employee's salary
-    var salary float64
-    for {
-        input := GetUserInput("Enter the salary of the new employee:")
-        if strings.HasPrefix(input, "-") {
-            log.Println("Salary cannot be negative.")
-            continue
-        }
+	// Get and validate the employee's salary
+	var salary float64
+	for {
+		input := GetUserInput("Enter the salary of the new employee:")
+		if strings.HasPrefix(input, "-") {
+			log.Println("Salary cannot be negative.")
+			continue
+		}
 
 		if input == "0" {
 			log.Println("Salary cannot be zero")
 			continue
 		}
-        var err error
-        salary, err = strconv.ParseFloat(input, 64)
-        if err != nil {
-            log.Println("Invalid salary:", err)
-            continue
-        }
-        break
-    }
+		var err error
+		salary, err = strconv.ParseFloat(input, 64)
+		if err != nil {
+			log.Println("Invalid salary:", err)
+			continue
+		}
+		break
+	}
 
-	return []string{name,fmt.Sprintf("%d",age),fmt.Sprintf("%.2f",salary)}
+	return []string{name, fmt.Sprintf("%d", age), fmt.Sprintf("%.2f", salary)}
 }
 
 func (d *department) AddEmployee() {
@@ -137,73 +139,76 @@ func (d *department) AddEmployee() {
 	}
 
 	age, _ := strconv.ParseUint(empDetails[1], 10, 32)
-	salary, _ := strconv.ParseFloat(empDetails[2],64)
-	d.employees = append(d.employees, employee{empDetails[0], uint(age), salary})
+	salary, _ := strconv.ParseFloat(empDetails[2], 64)
+	//
+	empId := len(d.employees) + 1
+	d.employees[empId] = employee{
+		name:   empDetails[0],
+		age:    uint(age),
+		salary: salary,
+	}
+
 	fmt.Println("Hang on! we are adding new employee details...")
 	time.Sleep(time.Millisecond * 1500)
 	EmployeeDetails(d)
-	
+
 }
 
 func (d *department) RemoveEmployee() {
 	empID := GetUserID(d, "Enter the employee ID whose record you want to delete: ")
-	
-	var newEmpDetails []employee
-	for ID, eachEmp := range d.employees {
-		if ID+1 != int(empID) {
-			newEmpDetails = append(newEmpDetails, eachEmp)
-		}
-	}
 
-	d.employees = newEmpDetails
-	fmt.Printf("Hang on! we are removing the employee details with ID%v...\n",empID)
+	delete(d.employees, int(empID))
+
+	fmt.Printf("Hang on! we are removing the employee details with ID%v...\n", empID)
 	time.Sleep(time.Millisecond * 1500)
 	EmployeeDetails(d)
 }
 
-func EmployeeDetails(d *department){
+func EmployeeDetails(d *department) {
+	keys := make([]int, 0, len(d.employees))
+	for key := range d.employees {
+		keys = append(keys, key)
+	}
+	sort.Ints(keys)
 	table := tablewriter.NewWriter(os.Stdout)
 	fmt.Println("====================================")
-	fmt.Println("\tDepartment -- ",d.name)
+	fmt.Println("\tDepartment -- ", d.name)
 	fmt.Println("====================================")
-	table.SetHeader([]string{"ID","Name","Age","Salary"})
-	for ID, eachEmp := range d.employees {
+	table.SetHeader([]string{"ID", "Name", "Age", "Salary"})
+	for _, ID := range keys {
 		table.Append([]string{
-			fmt.Sprintf("%d",ID+1),
-			eachEmp.name,
-			fmt.Sprintf("%d",eachEmp.age),
-			fmt.Sprintf("%.2f",eachEmp.salary),
+			fmt.Sprintf("%d", ID),
+			d.employees[ID].name,
+			fmt.Sprintf("%d", d.employees[ID].age),
+			fmt.Sprintf("%.2f", d.employees[ID].salary),
 		})
-	}	
+	}
 	table.Render()
 }
-
 
 func (d *department) GiveRaise() {
 
 	empID := GetUserID(d, "Enter the employee ID whom you want to give raise: ")
 
 	newSalary := 0.0
-	fmt.Printf("Enter the raise amount you want to give for the empID%v: ",empID)
-	fmt.Scanln(&newSalary)
-	var newEmpDetails []employee
-	for ID, eachEmp := range d.employees {
-		if ID+1 == int(empID) {
-			if newSalary < eachEmp.salary {
-				log.Printf("Raise amount need to be higher than old amount %v\n",eachEmp.salary)
-				return
-			} else {
-				eachEmp.salary = newSalary
+	for {
+		fmt.Printf("Enter the raise amount you want to give for the empID%v: ", empID)
+		fmt.Scanln(&newSalary)
+		if emp, exists := d.employees[int(empID)]; exists {
+			if newSalary < emp.salary {
+				log.Printf("Raise amount must be higher than the current salary: %.2f\n", emp.salary)
+				continue
 			}
+			emp.salary = newSalary
+			d.employees[int(empID)] = emp
+			break
 		}
-		newEmpDetails = append(newEmpDetails, eachEmp)
 	}
 
-	d.employees = newEmpDetails
-	fmt.Printf("Hang on! we are updating the salary of the EmpID%v...\n",empID)
+	fmt.Printf("Hang on! we are updating the salary of the EmpID%v...\n", empID)
 	time.Sleep(time.Millisecond * 1500)
 	EmployeeDetails(d)
-	
+
 }
 
 func (d *department) AverageSalary() {
@@ -215,18 +220,17 @@ func (d *department) AverageSalary() {
 		count++
 	}
 	average = sum / count
-	fmt.Printf("The average salary of %v department employees is: %.2f\n",d.name, average)
+	fmt.Printf("The average salary of %v department employees is: %.2f\n", d.name, average)
 }
 
+func Run() {
 
-func Run(){
-	
 	department1 := department{
 		name: "GoLang",
-		employees: []employee{
-			{"Arijit",22,11000.0},
-			{"Anirban",22,35000.0},
-			{"HrishiRaj",22,18000.0},
+		employees: map[int]employee{
+			1: {"Arijit", 22, 11000.0},
+			2: {"Anirban", 22, 35000.0},
+			3: {"HrishiRaj", 22, 18000.0},
 		},
 	}
 
@@ -238,10 +242,10 @@ func Run(){
 
 	department2 := department{
 		name: "DotNet",
-		employees: []employee{
-			{"Subham",22,30000.0},
-			{"Tanmoy",22,35000.0},
-			{"Mostafizur",22,18000.0},
+		employees: map[int]employee{
+			1: {"Subham", 22, 30000.0},
+			2: {"Tanmoy", 22, 35000.0},
+			3: {"Mostafizur", 22, 18000.0},
 		},
 	}
 
